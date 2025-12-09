e4633b2c209a84cdbcb9e935d7f9243fe05f1b6b
skgma1019 <qjdd1122@gmail.com>
2025-12-09 11:45:02 +0900

    DB모델

---
+++ backend/models/Movie.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db"); // 위에서 만든 연결 가져오기
+
+const Movie = sequelize.define(
+  "Movie",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    genreIds: {
+      type: DataTypes.JSON, // 핵심: 배열 저장용
+      allowNull: true,
+    },
+    winCount: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    matchCount: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: "Movies",
+  }
+);
+
+module.exports = Movie;
